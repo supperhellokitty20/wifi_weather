07a4933acab937da985b33022e4374c7fcb977cd
bunbo_tainam <macbookpro@Slave-Slx.local>
2021-07-15 18:12:26 +0700

    Finnish product

--- src/main.cpp
+++ src/main.cpp
@@ -6,104 +6,125 @@
 #include <ESP8266HTTPClient.h> 
 #include <WiFiClient.h> 
 #include <ESP8266WiFi.h>  
+#include <stdio.h> 
 #include <String> 
 
 
 #define API_KEY "e544484d503214178d65887b620d784c"  
-#define SSID  "Tien Thao 01 " 
+#define SSID  "Tien Thao 01" 
 #define PW "88889999" 
 #define city "Porto" 
 #define countryCode "PT" 
-// THE DEFAULT TIMER IS SET TO 10 SECONDS FOR TESTING PURPOSES
- // For a final application, check the API call limits per hour/minute to avoid getting blocked/banned
-unsigned long lastTime = 0;
- // Timer set to 10 minutes (600000)
- //unsigned long timerDelay = 600000;
- // Set timer to 10 seconds (10000)
-unsigned long timerDelay = 10000;
 double temp ; 
-double humid   ;  
-
-String jsonBuffer ; 
-
+double humid ; 
+// THE DEFAULT TIMER IS SET TO 10 SECONDS FOR TESTING PURPOSES
+// For a final application, check the API call limits per hour/minute to avoid getting blocked/banned
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 10 seconds (10000)
+unsigned long timerDelay = 10000;
 LiquidCrystalI2C_RS_EN(lcd,0x27,false) ;
 
-void setup(){ 
-    //Set up lcd 
-    Serial.begin(9600) ; 
-    Wire.begin() ; 
-    lcd.begin(16,2) ; 
-    //Set up wifi 
-    WiFi.begin(SSID,PW) ; 
-    while(WiFi.status() !=WL_CONNECTED){ 
-        delay(500) ; 
-        Serial.print("_") ; 
-    } 
-    Serial.println("Connected to wifi") ; 
-    Serial.println(String(WiFi.localIP())) ;  
-    //Setting up lcd  
-    taskManager.scheduleFixedRate(timerDelay,[]{ 
-        //Print temp 
-        lcd.setCursor(0,0) ;         
-        lcd.print(F("TEMP")) ;
-        lcd.print(temp) ; 
-        //Print humid 
-        lcd.setCursor(0,1) ;         
-        lcd.print(F("HIMID")) ;
-        lcd.print(humid) ; 
-        lcd.print("%") ; 
-    }) ;  
-} 
-// Handling get requqesst  
-String  get_request(const char* serverName ) { 
-    WiFiClient client ; 
-    HTTPClient http ;  
-    http.begin(client ,serverName) ; 
-    int  resp_code = http.GET() ; 
-    String payload = "{}" ; 
-    // If valid code 
-    if(resp_code>0) { 
-        Serial.print("Response code") ; 
-        Serial.println(resp_code)  ;
-        payload = http.getString() ;  
-    } 
-    else { 
-        Serial.print("Error") ; 
-        Serial.println(resp_code) ; 
-    } 
+String jsonBuffer;
 
-    http.end() ;
-    return payload ;
-} 
-// Getting info from the server 
-void  get_info() { 
-   if( ( millis()-lastTime) > timerDelay) { 
-            //Your server name 
-          String serverPath = (String) "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + countryCode + "&APPID=" + API_KEY;
-            //Get data from the server 
-            jsonBuffer = get_request(serverPath.c_str()) ; 
-            Serial.println(jsonBuffer) ; 
-            JSONVar json_info = JSON.parse(jsonBuffer) ; 
-            //Parsing failed 
-            if(JSON.typeof(json_info)=="undefined")  { 
-                Serial.println("parse failed") ;
-                return ; 
-            }             
-            // Handling your json info here 
-            // Adjust to match your type 
-          /**  
-            temp = json_info["main"]["feels_like"]  ;              
-            humid= json_info["main"]["humidity"]   ;
-            **/
-            Serial.println(json_info["main"]["feels_like"]) ; 
-            Serial.println(json_info["main"]["humidity"]); 
-        } 
-        lastTime = millis() ; 
+void setup() {
+  Serial.begin(9600);
+  Wire.begin() ; 
+  lcd.begin(16,2);
+  WiFi.begin(SSID, PW);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Timer set to 10 seconds (timerDelay variable), it will take 10 seconds before publishing the first reading.");
+    //Setting up the lcd 
+    taskManager.scheduleFixedRate(250, [] {
+    // (note: line 1 is the second row, since counting begins with 0):
+        lcd.noBlink();
+        lcd.setCursor(0,0) ;
+        lcd.print(F("Temp:")); 
+        lcd.print(temp) ;
+        // print the number of seconds since reset:
+        //float secondsFraction =  millis() / 1000.0F;
+        lcd.setCursor(0, 1);
+        lcd.print(F("Humid:")); 
+        lcd.print(humid);
+        lcd.print(F("%"));
+  });
+}
 
-   }  
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your IP address with path or Domain name with URL path 
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "{}"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
 
-void loop() { 
-    // This will set the value for temp and humid var   
-    get_info() ; 
-} 
+  return payload;
+}
 
+void loop() {
+  // Send an HTTP GET request
+  if ((millis() - lastTime) > timerDelay) {
+    // Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      String  serverPath = "http://api.openweathermap.org/data/2.5/weather?q=" city  ","  countryCode "&APPID=" API_KEY;
+      
+      jsonBuffer = httpGETRequest(serverPath.c_str());
+      Serial.println(jsonBuffer);
+      JSONVar myObject = JSON.parse(jsonBuffer);
+  
+      // JSON.typeof(jsonVar) can be used to get the type of the var
+      if (JSON.typeof(myObject) == "undefined") {
+        Serial.println("Parsing input failed!");
+        return;
+      }
+        temp =   myObject["main"]["temp"];
+        humid=  myObject["main"]["humidity"]; 
+      Serial.print("JSON object = ");
+      Serial.println(myObject);
+      Serial.print("Temperature: ");
+      Serial.println(temp);
+      Serial.print("Humidity: ");
+      Serial.println(humid);
+      /** 
+      Serial.println(myObject["main"]["temp"]);
+      Serial.print("Pressure: ");
+      Serial.println(myObject["main"]["pressure"]);
+      Serial.print("Humidity: ");
+      Serial.println(myObject["main"]["humidity"]);
+      Serial.print("Wind Speed: ");
+      Serial.println(myObject["wind"]["speed"]);
+      **/ 
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+    taskManager.runLoop() ;
+  }
+
+}
